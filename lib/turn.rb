4cf5cf3264a7a096eede1ba869458230c4c0b5a4
Sankalp Chauhan <sankalpchauhan.me@gmail.com>
2018-06-14 15:52:01 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -36,8 +36,7 @@ def turn(board)
   val = gets.strip
   index  = input_to_index(val)
 
-#  if(valid_move?(board,index)=="false")
-    until valid_move?(board,index)== "false"
+  if(valid_move?(board,index)=="false")
       turn(board)
     end
 #  end
