2b2494823651027599cb4a9d06c1492a73ed1318
Sankalp Chauhan <sankalpchauhan.me@gmail.com>
2018-06-14 15:20:33 +0000

    automatically backed up by learn

---
+++ lib/turn.rb
@@ -0,0 +1,32 @@
+ddef display_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
+
+def input_to_index(str)
+  num = str.to_i
+  num-1
+end
+
+def move(board, index, value="X")
+  board[index] = value
+end
+
+def position_taken?(board, index)
+  if(board[index] == " " || board[index] == "" || board[index] == nil)
+  return false
+  elsif(board[index] == "X" || board[index] == "O")
+  return true
+    end
+end
+
+def valid_move?(board, index)
+  if(index.between?(0,8) && !position_taken?(board,index))
+    return true
+  else
+    return false
+  end
+end
